468025910c3a22c284ce27816bd86bdfb1198eb3
NicoVIII <nico@nicoviii.net>
2021-10-30 13:34:27 +0000

    Add watcher stuff

--- run.fs
+++ run.fs
@@ -1,23 +1,53 @@
+open System
+
 open RunHelpers
+open RunHelpers.Watch
 
 [<RequireQualifiedAccess>]
 module Config =
     let project = "./src/RunHelpers.fsproj"
 
 module Task =
-    let restore () = Template.DotNet.restore Config.project
+    let restore () =
+        job {
+            Template.DotNet.toolRestore ()
+            Template.DotNet.restore Config.project
+        }
 
     let build = Template.DotNet.build Config.project
+
+    let watch () =
+        let options =
+            WatcherOptions.create ()
+            |> WatcherOptions.excludeFolders [ "bin"
+                                               "obj" ]
+
+        use watcher =
+            setupWatcher options [ "src" ] (fun () -> build Template.DotNet.Config.Debug)
+
+        printfn "Waiting for changes... (enter for exit)"
+        Console.ReadLine() |> ignore
+        Ok
 
     let pack = Template.Paket.pack Config.project
 
 module Command =
     let restore () = Task.restore ()
 
+    let subbuild () = Task.build Template.DotNet.Config.Debug
+
     let build () =
         job {
             restore ()
-            Task.build Template.DotNet.Config.Debug
+            subbuild ()
+        }
+
+    let subwatch () = Task.watch ()
+
+    let watch () =
+        job {
+            restore ()
+            subwatch ()
         }
 
     let pack version =
@@ -33,8 +63,11 @@ let main args =
     |> List.ofArray
     |> function
         | [ "restore" ] -> Command.restore ()
+        | [ "subbuild" ] -> Command.subbuild ()
         | []
         | [ "build" ] -> Command.build ()
+        | [ "subwatch" ] -> Command.subwatch ()
+        | [ "watch" ] -> Command.watch ()
         | [ "pack"; version ] -> Command.pack version
         // Missing args cases
         | [ "pack" ] ->
---
+++ src/Watch.fs
@@ -0,0 +1,110 @@
+namespace RunHelpers
+
+open System
+open System.IO
+
+/// Contains helpers to setup watch commands
+module Watch =
+    /// Helpertype for multiple FileSystemWatchers
+    type FileSystemWatcherList =
+        { watchers: FileSystemWatcher list }
+        interface IDisposable with
+            member this.Dispose() =
+                this.watchers
+                |> List.iter (fun watcher -> watcher.Dispose())
+
+    module FileSystemWatcherList =
+        let create watchers = { watchers = watchers }
+
+        let combine watcherLists =
+            watcherLists
+            |> List.collect (fun lst -> lst.watchers)
+            |> create
+
+    type WatcherOptions =
+        { includeSubdirectories: bool
+          excludeFolders: string list }
+
+    module WatcherOptions =
+        let create () =
+            { includeSubdirectories = true
+              excludeFolders = [ "bin"; "obj" ] }
+
+        let excludeFolders folders options =
+            { options with
+                excludeFolders =
+                    List.append options.excludeFolders folders
+                    |> List.distinct }
+
+        let excludeFolder folder = excludeFolders [ folder ]
+
+        let withoutSubdirectories options =
+            { options with includeSubdirectories = false }
+
+    let setupWatcher options folders onChange =
+        let mutable working = false
+        let mutable changedWhileWorking = false
+
+        let disable (watcher: FileSystemWatcher) = watcher.EnableRaisingEvents <- false
+        let enable (watcher: FileSystemWatcher) = watcher.EnableRaisingEvents <- true
+
+        let watchers =
+            folders
+            |> List.map (fun folder ->
+                let watcher = new FileSystemWatcher(folder)
+                watcher.IncludeSubdirectories <- options.includeSubdirectories
+                watcher)
+
+        /// Does the work necessary after a change
+        let rec work () =
+            working <- true
+            onChange () |> ignore
+
+            if changedWhileWorking then
+                changedWhileWorking <- false
+                printfn "- Do it again, there were changes while running"
+                work ()
+            else
+                printfn "- Waiting for changes... (enter to exit)"
+                working <- false
+
+        /// Handles events, debounces them and filters them
+        let handler (args: FileSystemEventArgs) =
+            let filtered =
+                options.excludeFolders
+                |> List.exists (args.FullPath.Contains)
+
+            if not filtered then
+                List.iter disable watchers
+
+                let working =
+                    async {
+                        if working then
+                            changedWhileWorking <- true
+                        else
+                            work ()
+                    }
+
+                let debouncer =
+                    async {
+                        do! Async.Sleep(500)
+                        List.iter enable watchers
+                    }
+
+                [ working; debouncer ]
+                |> Async.Parallel
+                |> Async.Ignore
+                |> Async.RunSynchronously
+
+        // Register handler
+        List.iter
+            (fun (watcher: FileSystemWatcher) ->
+                watcher.Changed.Add handler
+                watcher.Created.Add handler
+                watcher.Deleted.Add handler)
+            watchers
+
+        // Enable Watchers
+        List.iter enable watchers
+
+        FileSystemWatcherList.create watchers
